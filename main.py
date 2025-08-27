18f0fe0324419713d166a764c6f7652d0bddb3a6
robotitalia <patrispa@proton.me>
2025-08-27 15:00:07 +0200

    .

--- main.py
+++ main.py
@@ -10,13 +10,15 @@ TARGET_URL = "https://apistg.lavoro.gov.it/InformationDelivery/SmartWorking_Bulk
 async def proxy(request: Request):
     # Costruisci URL completo
     url = f"{TARGET_URL}/creaComunicazioni"
+    print("request: ", request)
 
     # Copia headers (escludendo host)
     headers = {k: v for k, v in request.headers.items() if k.lower() != "host"}
-    print("Headers: ", headers)
+    # print("Headers: ", headers)
+
     # Corpo della richiesta
     body = await request.body()
-    console.log("Body: ", body)
+    # print("Body: ", body)
     
     # Inoltra la chiamata al target usando httpx
     async with httpx.AsyncClient(verify=False) as client:  # verify=False per self-signed
